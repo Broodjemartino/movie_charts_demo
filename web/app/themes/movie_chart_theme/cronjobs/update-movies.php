1665253eac642049ebd94c5982bb7f5d6b4da975
Broodjemartino <154263527+Broodjemartino@users.noreply.github.com>
2023-12-27 08:35:47 +0100

    Added wp cron

--- web/app/themes/movie_chart_theme/cronjobs/update-movies.php
+++ web/app/themes/movie_chart_theme/cronjobs/update-movies.php
@@ -2,7 +2,14 @@
 
 use src\ApiConnetionTmdb;
 
-$connection = new ApiConnetionTmdb();
-$connection->updateMovies();
+if (! wp_next_scheduled('mcd_update_movies_every_hour')) {
+    //cronjob sync every hour
+    wp_schedule_event(time(), 'hourly', 'mcd_update_movies_every_hour');
+}
 
-?>
+add_filter('mcd_update_movies_every_hour', 'mcd_get_movies_from_api');
+function mcd_get_movies_from_api()
+{
+    $connection = new ApiConnetionTmdb();
+    $connection->updateMovies();
+}
